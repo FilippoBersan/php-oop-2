3af38c76282bf29773d4b452749b6790272d5cc0
FilippoBersan <filippobersan99@gmail.com>
2024-01-16 16:38:31 +0100

    feat: construct ereditarietà

--- index.php
+++ index.php
@@ -1,5 +1,58 @@
 <?php 
+
+
+class Prodotti{
+  public $prodotti;
+
+  function __construct($_prodotti)
+  {
+    $this->prodotti = $_prodotti;
+  }
+}
+
+
+class Categorie extends Prodotti{
+  public $cani;
+  public $gatti;
+
+  function __construct($prodotti,$_cani,$_gatti)
+  {
+    parent::__construct($prodotti);
+
+        $this->cani = $_cani;
+            $this->gatti = $_gatti;
+  }
+}
+
+class Vari extends Categorie{
+  public $cibo;
+  public $giochi;
+  public $cucce;
+
+
+  function __construct($prodotti,$_cani,$_gatti,$_cibo,$_giochi,$_cucce)
+  {
+
+    parent::__construct($prodotti,$_cani,$_gatti);
+    $this->cibo = $_cibo;
+      $this->giochi = $_giochi;
+        $this->cucce = $_cucce;
+  }
+}
+
+
+$prodotti= new Prodotti('Prodotti');
+$categorie = new Categorie('Prodotti','Cani','Gatti');
+$vari = new Vari('Prodotti','Cani','Gatti','Cibo','Giochi','Cucce');
+
+
+
+var_dump($prodotti,$categorie,$vari);
 ?>
+
+
+
+
 
 
 
